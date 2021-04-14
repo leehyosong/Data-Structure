dbb941a4831f1b65ea080820f547e06b6be82418
leehyosong <74747289+leehyosong@users.noreply.github.com>
2021-04-15 02:31:14 +0900

    Add files via upload

---
+++ week06/bracketTest/BraketTest.java
@@ -0,0 +1,19 @@
+package bracketTest;
+
+public class BraketTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OptExp opt = new OptExp();
+		String exp = "(3*5)-(6/2)";
+		
+		System.out.println(exp);
+		
+		if(opt.testPair(exp))
+			System.out.println("°ýÈ£ ¸ÂÀ½!!");
+		else
+			System.out.println("°ýÈ£ Æ²¸²!!");
+
+	}
+
+}
